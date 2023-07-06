22e6c4935cf0fc73c926946140b55763a33399db
Jaden <yjp9603@gmail.com>
2023-07-07 03:35:42 +0900

    Feat: 댓글 도메인 생성

---
+++ src/domains/chats/entities/chat.entity.ts
@@ -0,0 +1,25 @@
+import BaseEntity from 'src/base.entity';
+import { Post } from 'src/domains/posts/entities/post.entity';
+import { User } from 'src/domains/users/entities/user.entity';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+
+@Entity()
+export class Chat extends BaseEntity {
+  @Column({
+    nullable: false,
+  })
+  content: string;
+
+  @ManyToOne(() => Post)
+  post: Post;
+
+  @ManyToOne(() => User)
+  author: User;
+
+  @JoinColumn({ name: 'parent_id' })
+  @ManyToOne(() => Chat)
+  private parent: Chat;
+
+  @OneToMany(() => Chat, (child) => child.parent)
+  private child: Chat[];
+}
