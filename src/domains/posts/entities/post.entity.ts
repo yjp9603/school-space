10b584ea3347ffe598ee51da3a9d7a5e71488225
Jaden <yjp9603@gmail.com>
2023-07-07 03:11:43 +0900

    Feat: 게시글 도메인 및 엔티티 관계 생성

---
+++ src/domains/posts/entities/post.entity.ts
@@ -0,0 +1,12 @@
+import { User } from './../../users/entities/user.entity';
+import { Entity, ManyToOne } from 'typeorm';
+import BaseEntity from 'src/base.entity';
+import { Space } from 'src/domains/spaces/entities/space.entity';
+@Entity()
+export class Post extends BaseEntity {
+  @ManyToOne(() => User)
+  author: User;
+
+  @ManyToOne(() => Space)
+  space: Space;
+}
